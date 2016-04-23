b29d020d0ee9df25e937914c3ea9a8e7768177b6
Max Harley <augeocacher@gmail.com>
2016-04-23 18:41:27 -0400

    ha

--- WebServer/routes/index.js
+++ WebServer/routes/index.js
@@ -2,7 +2,15 @@ var express = require('express');
 var Points = require(__dirname + '/../models/Points.js');
 var config = require(__dirname + '/../config.js');
 var router = express.Router();
-
+/*
+ @@DNA
+  Description: An express route to index. It gets all of the points in the Points database and renders
+  the points in the front end.
+  Input: limitFactor under req.query.limit. It limits the amount of SELECTS we do from the DB
+  Output: Render the 'index' page with 'points', which comes from the database, and 'maps_api' which
+  comes from config.mapsAPI.
+ @@END
+ */
 router.get('/', function(req, res) {
   var limitFactor = req.query.limit || 100;
   Points
@@ -18,20 +26,44 @@ router.get('/', function(req, res) {
     for(var i in points){
       pointsArr.push({ "time": points[i].time, "lat": points[i].lat, "lng": points[i].lng, "UID": points[i].UID });
     }
-    res.render('index', { points: pointsArr || {}, map_api: config.mapsAPI, error: err });
+    res.render('index', { points: pointsArr || {}, map_api: config.mapsAPI});
   });
 });
 
+/*
+ @@DNA
+ Description: An express post to '/addWaypoint'. It adds a waypoint to the Points DB. It uses an API key
+ for security. The true API key is an environment variable named APIKEY.
+ Input: req.body.APIKEY - User-submitted API key
+        req.body.waypoint - Longitude and lattitude split by a comma
+        req.body.UID - The UID for the specified trip
+ Output: Point{ UID, lng, lat, time } and a success and error message
+ @@END
+ */
 router.post('/addWaypoint', function(req, res){
   var serverPassword = process.env.APIKEY;
   var userPassword = req.body.APIKEY;
-  
+
   console.log("\n\n\n");
   console.log(req.body);
   console.log("\n\n\n");
-  // waypoint - -127.00123,123.2394
-  // UID  - 1oi4sf712fjh
 
+  if(!req.body.waypoint){
+    return res.status(400).send({
+      message: "No waypoint"
+    });
+  }
+  if(!req.body.APIKEY){
+    return res.status(400).send({
+      message: "No API Key"
+    });
+  }
+  if(!req.body.UID){
+    return res.status(400).send({
+      message: "No UID specified"
+    });
+  }
+  
   var waypoint = req.body.waypoint;
   var lat = waypoint.split(',')[0];
   var lng = waypoint.split(',')[1];
